7fbc05a0ca46c72367cad4ab3b508581764daf1d
martin-kloeppner <martin.kloeppner@lotto24.de>
2023-06-30 05:25:41 +0000

    WSP-4480: Async/await cleaner code

--- libs/imports-orchestrator/src/lib/host-directive/import-loader.directive.ts
+++ libs/imports-orchestrator/src/lib/host-directive/import-loader.directive.ts
@@ -16,45 +16,44 @@ export class ImportsOrchestratorLoaderDirective implements AfterViewInit {
 
   private running = 0;
 
-  public async ngAfterViewInit(): Promise<void> {
+  public ngAfterViewInit(): void {
     if (!ImportsOrchestratorLoaderDirective.processing) {
       // do not await, as it would block the lifecycle callback from completing until the queue is processed
       ImportsOrchestratorLoaderDirective.processing = true;
 
-      await this.processQueue();
-
-      this.config.logger.debug('queue processing ended');
-      ImportsOrchestratorLoaderDirective.processing = false;
+      this.processQueue()
+        .then(() => {
+          this.config.logger.debug('queue processing ended');
+        })
+        .catch(() => {
+          this.config.logger.debug('queue processing failed');
+        })
+        .finally(() => {
+          ImportsOrchestratorLoaderDirective.processing = false;
+        });
     }
   }
 
-  private processQueue(pid: number = 0): Promise<void> {
-    return new Promise<void>((resolve) => {
-      const currentBatch: Promise<void>[] = [];
-      for (let i = this.running; i < this.config.parallel; i++) {
-        this.running++;
-        currentBatch.push(
-          processImportItem(pid++, this.config, this.router)
-            .then(() => {
-              this.config.logger.debug(
-                `Queue resolved item, concurrent now ${this.running}`
-              );
-            })
-            .finally(() => {
-              this.running--;
-              if (this.config.queue.length !== 0) {
-                this.processQueue(pid++).then(() => {
-                  resolve();
-                });
-              } else if (this.running === 0) {
-                resolve();
-              }
-            })
-        );
-      }
-      this.config.logger.debug(
-        `Queue starting batch with ${currentBatch.length}, concurrent now ${this.running}`
-      );
-    });
+  private async processQueue(pid: number = 0): Promise<void> {
+    const concurrentBatch = [];
+    for (let i = this.running; i < this.config.parallel; i++) {
+      this.running++;
+      concurrentBatch.push(this.processItemAndContinueQueue(pid++));
+    }
+    this.config.logger.debug(
+      `Queue starting ${concurrentBatch.length} item(s) to reach max parallel, concurrent now ${this.running}`
+    );
+    await Promise.all(concurrentBatch);
+  }
+
+  private async processItemAndContinueQueue(pid: number): Promise<void> {
+    await processImportItem(pid++, this.config, this.router);
+    this.running--;
+    this.config.logger.debug(
+      `Queue item resolved, concurrent now ${this.running}`
+    );
+    if (this.config.queue.length !== 0) {
+      await this.processQueue(pid++);
+    }
   }
 }
