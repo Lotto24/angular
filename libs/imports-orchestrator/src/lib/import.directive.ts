118213fc1429d10cd1572f38ea2f300fdf43006a
Marco Steinhoff <marco.steinhoff@lotto24.de>
2023-07-03 06:00:12 +0000

    Create importQueued emitter

--- libs/imports-orchestrator/src/lib/import.directive.ts
+++ libs/imports-orchestrator/src/lib/import.directive.ts
@@ -12,7 +12,7 @@ import {
     {
       directive: ImportsOrchestratorQueueDirective,
       inputs: ['import', 'orderKey', 'providers', 'inputs', 'outputs'],
-      outputs: ['importFinished'],
+      outputs: ['importFinished', 'importQueued'],
     },
     {
       directive: ImportsOrchestratorClassDirective,
